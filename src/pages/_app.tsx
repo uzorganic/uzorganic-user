49eabff552fbdcbf41f46e36fb01f6038939a930
SunoopDogg <aswoo55555@gmail.com>
2024-07-13 01:05:48 +0900

    refactor: Refactor App component to support dynamic layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,36 @@
 import { client } from '@/api/reactQuery';
 import { QueryClientProvider } from '@tanstack/react-query';
 
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 
 import '@/styles/fonts.css';
 import '@/styles/globals.css';
 import '@/styles/value.css';
 
-import { WebLayout } from '@/layouts/web';
+import { ContentLayout } from '@/layouts/ContentLayout';
 import { antdThemeConfig } from '@/theme/config';
 import { ConfigProvider } from 'antd';
 import koKR from 'antd/locale/ko_KR';
 
-export default function App({ Component, pageProps }: AppProps) {
+type AppPropsWithLayout = AppProps & {
+  Component: {
+    layout: (page: React.ReactNode) => JSX.Element;
+    layoutFillColor?: string;
+  };
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <>
       <ConfigProvider locale={koKR} theme={antdThemeConfig}>
         <QueryClientProvider client={client}>
-          <WebLayout>
-            <Component {...pageProps} />
-          </WebLayout>
+          {Component.layout ? (
+            Component.layout(<Component {...pageProps} />)
+          ) : (
+            <ContentLayout fillColor={Component.layoutFillColor}>
+              {<Component {...pageProps} />}
+            </ContentLayout>
+          )}
         </QueryClientProvider>
       </ConfigProvider>
     </>
